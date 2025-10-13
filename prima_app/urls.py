dbe810ed31854d023a9af53fb5b680c2562c9035
Riccardo Antonini <165804693+FakeGTs@users.noreply.github.com>
2025-10-13 13:00:17 +0200

    Initial commit

---
+++ prima_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from prima_app.views import homepage,welcome,lista,chiSiamo
+
+app_name="prima_app"
+
+urlpatterns=[
+    path('', homepage, name="homepage"),
+    path('welcome', welcome, name="welcome"),
+    path('lista', lista, name="lista"),
+    path('chi_siamo', chiSiamo, name="chi_siamo"),
+]
