f9d830c5dcceb163b435e18e7c1426962da59b17
Riccardo Antonini <165804693+FakeGTs@users.noreply.github.com>
2025-11-03 13:17:17 +0100

    aggiornato al passo12 funzionante

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'prima_app',
+    'prima_app','seconda_app',
     
     
 ]
@@ -58,7 +58,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR, 'prima_app/templates/prima_app'),
-                 os.path.join(BASE_DIR, 'templates')],
+                 os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'seconda_app/templates/seconda_app')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
