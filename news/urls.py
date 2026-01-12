8e4e52d771da9a0a3d1dff85a3d669467b856631
Riccardo Antonini <165804693+FakeGTs@users.noreply.github.com>
2026-01-12 13:10:37 +0100

    finito news

--- news/urls.py
+++ news/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from news.views import home
+from news.views import homeview, ArticoloDetailView, index
 
 app_name="news"
 
 urlpatterns=[
-    path('', home , name="homeview"),
+    path('index', index , name="index"),
+    path('homeview', homeview , name="homeview"),
+    path("articoli/<int:pk>", ArticoloDetailView , name="articolo_detail"),
+
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import HttpResponse
-from .models import Artticolo,Giornalista
+from django.shortcuts import render, get_object_or_404
+from .models import Articolo,Giornalista
 
-def home(request):
-    return HttpResponse("<h1>Homepage news!</h1>")
+# def home(request):
+#     a=""
+#     g=""
+#     for art in Articolo.objects.all():
+#         a+=(art.titolo+"<br>")
+#     response = "Articoli: <br>"+ a + "<br>Giornalisti:<br>"+ g
+    
+#     return HttpResponse("<h1>"+ response +"</h1>")
+def index(request):
+    return render(request, "news/index.html")
+def homeview(request):
+    articoli =Articolo.objects.all()
+    giornalisti =Giornalista.objects.all()
+    context={"articoli" : articoli, "giornalisti" : giornalisti}
+    print(context)
+    return render(request, "homeview.html", context)
+
+def ArticoloDetailView(request, pk):
+    articolo=get_object_or_404(Articolo, pk=pk)
+    context={"articolo": articolo}
+    return render (request,"articolo_detail.html", context)
