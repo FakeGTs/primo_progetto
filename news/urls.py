191e1eedce3b7d7d8bf84f92684dc5f3d56a67af
Riccardo Antonini <165804693+FakeGTs@users.noreply.github.com>
2026-01-19 13:10:19 +0100

    lista articolo modificata

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from news.views import homeview, ArticoloDetailView, index
+from news.views import homeview, ArticoloDetailView, index, listaArticoli
 
 app_name="news"
 
 urlpatterns=[
     path('index', index , name="index"),
-    path('homeview', homeview , name="homeview"),
-    path("articoli/<int:pk>", ArticoloDetailView , name="articolo_detail"),
+    path('homeview', homeview , name="homeview"), 
+    path("articolo/<int:pk>", ArticoloDetailView , name="articolo_detail"),
+    path("lista_articoli", listaArticoli , name="lista_articoli"),
+    path("lista_articoli/<int:pk>", listaArticoli , name="lista_articoli"),
+
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Articolo,Giornalista
 
-# def home(request):
-#     a=""
-#     g=""
-#     for art in Articolo.objects.all():
-#         a+=(art.titolo+"<br>")
-#     response = "Articoli: <br>"+ a + "<br>Giornalisti:<br>"+ g
-    
-#     return HttpResponse("<h1>"+ response +"</h1>")
 def index(request):
     return render(request, "news/index.html")
 def homeview(request):
@@ -17,8 +9,26 @@ def homeview(request):
     context={"articoli" : articoli, "giornalisti" : giornalisti}
     print(context)
     return render(request, "homeview.html", context)
-
 def ArticoloDetailView(request, pk):
     articolo=get_object_or_404(Articolo, pk=pk)
     context={"articolo": articolo}
     return render (request,"articolo_detail.html", context)
+
+def listaArticoli(request,pk=None):
+    if(pk==None):
+        articoli=Articolo.objects.all()
+        giornalista=None
+    else:
+        articoli=Articolo.objects.filter(giornalista_id=pk)
+        giornalista=Giornalista.objects.get(pk=pk)
+    if(pk==None):
+        is_giornalista=False
+    else:
+        is_giornalista=True
+    context={
+        'articoli':articoli,
+        'is_giornalista':is_giornalista,
+        'giornalista':giornalista
+    }
+
+    return render(request, 'lista_articoli.html',context)
