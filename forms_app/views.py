9421478b55661b03edafbf046934c44ab31035b8
Riccardo Antonini <165804693+FakeGTs@users.noreply.github.com>
2026-03-16 11:47:33 +0100

    finita app contatti

--- forms_app/views.py
+++ forms_app/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, redirect, render
 from forms_app.models import Contatto
 from .forms import FormContatto
 from django.http import HttpResponse
+from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.decorators import login_required
 
 # def contatti(request): view precedente senza gestione del form
-#     form=FormContatto()
+#     form = FormContatto()
 #     context = {"form": form}
 #     return render(request, "contatto.html", context)
 
@@ -23,20 +24,49 @@ def contatti(request):
             print("Contenuto:", form.cleaned_data['contenuto'])
 
             print("Dati salvati nel database!")
-            nuovo_contatto= form.save()
-            print("nuovo post creato:", nuovo_contatto)
+            nuovo_contatto = form.save()
+
+            print("Nuovo contatto creato:", nuovo_contatto)
             print(nuovo_contatto.nome)
             print(nuovo_contatto.cognome)
             print(nuovo_contatto.email)
             print(nuovo_contatto.contenuto)
+
             return HttpResponse("<h1>Grazie per averci contattato!</h1>")
     else:
         form = FormContatto()
-    
     context = {"form": form}
     return render(request, "contatto.html", context)
+
 
 def lista_contatti(request):
     contatti = Contatto.objects.all()
     context = {"contatti": contatti}
     return render(request, "lista_contatti.html", context)
+
+@login_required(login_url="/accounts/login")
+def modifica_contatto(request, pk):
+    contatto = get_object_or_404(Contatto, id=pk)
+    if request.method == "POST":
+        form = FormContatto(request.POST, instance=contatto)
+        if form.is_valid():
+            form.save()
+            return redirect("forms_app:lista-contatti")
+    else:
+        form = FormContatto(instance=contatto)
+    context = {
+        "form": form,
+        "contatto": contatto
+    }
+    return render(request, "modifica_contatto.html", context)
+
+@staff_member_required(login_url="/accounts/login")
+def elimina_contatto(request, pk):
+    contatto = get_object_or_404(Contatto, id=pk)
+
+    if request.method == "POST":  # vuol dire che l'utente ha inviato il form che conferma l'eliminazione
+        contatto.delete()  # elimina il contatto dal database
+        return redirect('forms_app:lista-contatti')
+
+    context = {'contatto': contatto}
+    return render(request, 'elimina_contatto.html', context)
